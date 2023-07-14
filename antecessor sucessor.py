b6d7bb91f7036d44e96f637f97926d232194dcba
SouReiDavi <134940821+SouReiDavi@users.noreply.github.com>
2023-07-14 09:26:35 -0300

    Create antecessor sucessor.py

---
+++ antecessor sucessor.py
@@ -0,0 +1,13 @@
+num1 = float (input("Informe o primeiro valor"))
+
+num2 = float (input("Informe o segundo valor"))
+
+antecessor = -1
+sucessor  = +1
+
+print ("antecesor: de {} e {}".format(num1,antecessor))
+print ("sucessor: de {} e {}".format(num2,sucessor))
+
+totalsoma = num1 + num2
+
+print(totalsoma)
